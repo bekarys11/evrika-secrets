f49501e40d50a754eb4f17b5aa49d42094154c8c
bekarys11 <ibekarys@gmail.com>
2023-11-13 17:35:49 +0600

    feat: added roles, users' roles and users' secrets migrations

---
+++ pkg/migrations/5_create_users_secrets_table.up.sql
@@ -0,0 +1,7 @@
+	CREATE TABLE IF NOT EXISTS users_secrets (
+	    user_id INT,
+	    secret_id INT,
+	    PRIMARY KEY (user_id, secret_id),
+        CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id),
+        CONSTRAINT fk_role FOREIGN KEY (secret_id) REFERENCES secrets (id)
+    )
