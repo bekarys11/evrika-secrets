a7eb0cb8b70f1a5dea9a68acddf6e741b8a6c249
bekarys11 <ibekarys@gmail.com>
2023-11-16 11:36:09 +0600

    fix: changed order of migrations and added role id to users table

--- pkg/migrations/4_create_users_roles_table.up.sql
+++
@@ -1,7 +0,0 @@
-	CREATE TABLE IF NOT EXISTS users_roles (
-	    user_id INT,
-	    role_id INT,
-	    PRIMARY KEY (user_id, role_id),
-        CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id),
-        CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles (id)
-    )
