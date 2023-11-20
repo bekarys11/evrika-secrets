f4e127b0297286f72569f465c80274dd76646891
bekarys11 <ibekarys@gmail.com>
2023-11-20 15:53:38 +0600

    fix: seed

--- cmd/seeder/seed.go
+++ cmd/seeder/seed.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	"golang.org/x/crypto/bcrypt"
+	"log"
 	"os"
 )
 
 func PopulateRoles(db *sqlx.DB) error {
+	//if _, err := db.Exec("TRUNCATE TABLE roles RESTART IDENTITY;"); err == nil {
+	//	return fmt.Errorf("error truncating roles table: %v", err)
+	//}
+
 	_, err := db.Exec(`
 	INSERT INTO roles (name, alias) 
 	VALUES ('Админ', 'admin'), ('Пользователь', 'user')
@@ -20,18 +25,20 @@ func PopulateRoles(db *sqlx.DB) error {
 }
 
 func PopulateUsers(db *sqlx.DB) error {
+	log.Println(os.Getenv("USER_PASSWORD"))
 	hashed, err := bcrypt.GenerateFromPassword([]byte(os.Getenv("USER_PASSWORD")), bcrypt.DefaultCost)
 	if err != nil {
 		return fmt.Errorf("error generating password for user: %v", err.Error())
 	}
 
+	//if _, err := db.Exec("TRUNCATE TABLE users RESTART IDENTITY;"); err == nil {
+	//	return fmt.Errorf("error truncating roles table: %v", err)
+	//}
+
 	_, err = db.Exec(`
 		INSERT INTO users (name, email, password, is_active, role_id) 
 		VALUES ($1, $2, $3, $4, $5)
-		ON CONFLICT (role_id)
-		DO
-			UPDATE SET password = $3;
-		`, "bekarys", "bekarys.t@evrika.com", string(hashed), true, 1)
+		ON CONFLICT DO NOTHING`, "bekarys", "bekarys.t@evrika.com", string(hashed), true, 1)
 	if err != nil {
 		return fmt.Errorf("error creating user: %v", err.Error())
 	}
