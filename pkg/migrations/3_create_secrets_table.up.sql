6259cb0368049c38b86e0b49c6b27aba8950f448
bekarys11 <ibekarys@gmail.com>
2023-12-04 07:46:10 +0600

    fix: added type field to migrations sql

--- pkg/migrations/3_create_secrets_table.up.sql
+++ pkg/migrations/3_create_secrets_table.up.sql
@@ -1,12 +1,15 @@
-	CREATE TABLE IF NOT EXISTS secrets (
+DROP TYPE types CASCADE;
+CREATE TYPE types AS ENUM ('auth', 'env', 'ssh');
+    CREATE TABLE IF NOT EXISTS secrets (
     id SERIAL UNIQUE,
     title VARCHAR (100),
 	key VARCHAR (100) UNIQUE NOT NULL,
     data TEXT,
+    stype types,
 	author_id INT,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT fk_author
 	FOREIGN KEY (author_id)
     REFERENCES users(id) ON DELETE NO ACTION ON UPDATE CASCADE
-    )
+)
